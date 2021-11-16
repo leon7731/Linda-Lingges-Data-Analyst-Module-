15c8abc6ff8a023312c23156c39cc3af4e9c2926
Lingges Arulsamy <38613094+leon7731@users.noreply.github.com>
2021-11-16 20:11:35 +0800

    Add files via upload

---
+++ Linda_Main.py
@@ -0,0 +1,15 @@
+from Data_Analysis import Data_Analysis as DA
+from Missing_Data_Analysis import Missing_Data_Analysis as MDA
+from Data_Visualization import Data_Visualization as DV
+
+
+class Data_Analysis(DA):
+    pass
+
+
+class Missing_Data_Analysis(MDA):
+    pass
+
+
+class Data_Visualization(DV):
+    pass
